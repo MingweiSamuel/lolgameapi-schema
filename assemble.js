d75cf0be39931e30d9fbb787143b90e419211e7d
Mingwei Samuel <mingwei.samuel@gmail.com>
2020-08-29 22:44:52 -0700

    Adding operationId names

--- assemble.js
+++ assemble.js
@@ -3,17 +3,19 @@ const fs = require('fs');
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const dtos = require('./dtos');
-const returnTypes = require('./returnTypes');
+const extraData = require('./extraData');
 const openapi = require('./openapi');
 
 openapi.components.schemas = dtos;
 openapi.paths = Object.fromEntries(Object.entries(openapi.paths)
     .filter(([ path, pathInfo ]) => {
-        const returnType = returnTypes[path];
-        if (undefined === returnType) return false;
-        pathInfo['x-endpoint'] = 'LiveClientData'; // TODO if there's more than one.
-        if (null !== returnType)
-            pathInfo.get.responses['200'].content['application/json'].schema = returnType;
+        const extraDatum = extraData[path];
+        if (undefined === extraDatum) return false;
+        // TODO handle endpoints properly.
+        pathInfo['x-endpoint'] = 'LiveClientData';
+        pathInfo.get.operationId = 'LiveClientData.get' + extraDatum.name;
+        if (extraDatum.retval)
+            pathInfo.get.responses['200'].content['application/json'].schema = extraDatum.retval;
         return true;
     }));
 openapi.info.description = 'League of Legends game client Live Client Data API';
